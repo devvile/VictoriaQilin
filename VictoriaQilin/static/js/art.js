81118242893281a3451486afccca311a675836a4
Dottore <patryk.czemierowski@gmail.com>
2020-08-17 20:40:19 +0200

    sociale_icons animation

--- VictoriaQilin/static/js/art.js
+++ VictoriaQilin/static/js/art.js
@@ -3,6 +3,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 //ELEMENTS
 
+//FIRST PAGE
+
 const logoV = document.getElementById("logoV");
 const logoQ = document.getElementById("logoQ");
 const welcome_text = document.querySelector(".welcome_text");
@@ -10,6 +12,10 @@ const navbar = document.querySelector(".navbar");
 const arrowRight = document.querySelector(".bar_arrow_right");
 const shopThumb = document.querySelector(".shopThumb");
 const shopText = document.querySelector(".shopText");
+const instaIcon = document.querySelector(".sociale__insta");
+const fbIcon = document.querySelector(".sociale__fb");
+
+//OTHERS
 const s3 = document.querySelector(".s3");
 
 //ENTER GALLERY ELEMENTS
@@ -60,15 +66,20 @@ gsap.fromTo(
     },
   }
 );
+
 //SETUP
-TweenMax.set([logoV, logoQ, welcome_text, shopText, arrowRight], {
-  visibility: "visible",
-});
+TweenMax.set(
+  [logoV, logoQ, welcome_text, shopText, arrowRight, fbIcon, instaIcon],
+  {
+    visibility: "visible",
+  }
+);
 TweenMax.set([navbar, shopThumb, shopText], {
   visibility: "visible",
   x: 430,
 });
-
+TweenMax.set(instaIcon, { x: 1000 });
+TweenMax.set(fbIcon, { x: -1000 });
 //FUNC
 function unblock() {
   setTimeout(() => {
@@ -81,11 +92,12 @@ TweenMax.to(logoV, 1.5, { opacity: 1 });
 TweenMax.to(logoQ, 1.5, { opacity: 1, delay: 1 });
 TweenMax.to(welcome_text, 1.5, { opacity: 1, delay: 2 });
 TweenMax.to(navbar, 0.75, { x: 0, delay: 3 });
-TweenMax.to(shopThumb, 0.75, { x: -15, delay: 3.5 });
-TweenMax.to(shopThumb, 0.2, { x: 40, delay: 4.15 });
+TweenMax.to(shopThumb, 0.75, { x: -15, delay: 3.5, ease: "easeIn" });
+TweenMax.to(shopThumb, 0.2, { x: 40, delay: 4.15, ease: "easeInOut" });
 TweenMax.to(shopThumb, 0.3, { x: 0, delay: 4.3 });
 TweenMax.to(shopText, 0.5, { x: 0, delay: 4.8 });
 TweenMax.to(arrowRight, 0.5, { opacity: 1, delay: 5.3 });
+TweenMax.to([fbIcon, instaIcon], 0.5, { x: 0, delay: 5.5, ease: "easeOut" });
 
 //SCROLL ANIM GALLERY
 
