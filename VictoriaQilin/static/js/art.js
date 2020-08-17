e6aece4220c7e521c537a88b3f618f0b3123aaa7
Dottore <patryk.czemierowski@gmail.com>
2020-08-17 12:43:07 +0200

    initial commit

---
+++ VictoriaQilin/static/js/art.js
@@ -0,0 +1,31 @@
+//ELEMENTS
+
+const logoV = document.getElementById("logoV");
+const logoQ = document.getElementById("logoQ");
+const welcome_text = document.querySelector(".welcome_text");
+const navbar = document.querySelector(".navbar");
+const arrowRight = document.querySelector(".bar_arrow_right");
+const shopThumb = document.querySelector(".shopThumb");
+const shopText = document.querySelector(".shopText");
+
+//ANIMATIONS GSAP
+
+//SETUP
+TweenMax.set([logoV, logoQ, welcome_text, shopText, arrowRight], {
+  visibility: "visible",
+});
+TweenMax.set([navbar, shopThumb, shopText], {
+  visibility: "visible",
+  x: 430,
+});
+
+//ANIM
+TweenMax.to(logoV, 1.5, { opacity: 1 });
+TweenMax.to(logoQ, 1.5, { opacity: 1, delay: 1 });
+TweenMax.to(welcome_text, 1.5, { opacity: 1, delay: 2 });
+TweenMax.to(navbar, 0.75, { x: 0, delay: 3 });
+TweenMax.to(shopThumb, 0.75, { x: -15, delay: 3.5 });
+TweenMax.to(shopThumb, 0.2, { x: 40, delay: 4.15 });
+TweenMax.to(shopThumb, 0.3, { x: 0, delay: 4.3 });
+TweenMax.to(shopText, 0.5, { x: 0, delay: 4.8 });
+TweenMax.to(arrowRight, 0.5, { opacity: 1, delay: 5.3 });
