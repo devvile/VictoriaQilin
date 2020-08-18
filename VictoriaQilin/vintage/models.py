47abc81ef09e52cfc5a69c6e08362ce286b4b3c8
Dottore <patryk.czemierowski@gmail.com>
2020-08-18 15:14:45 +0200

    new model - Ciuch, pillow lib

--- VictoriaQilin/vintage/models.py
+++ VictoriaQilin/vintage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Ciuch(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    size = models.CharField(max_length=4)
+    description = models.TextField(max_length=1000)
+    main_img = models.ImageField(upload_to='uploads/user/', null=True, default=None)
+
