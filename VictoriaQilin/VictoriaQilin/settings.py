e2ce2331baeb39b266f47da8e4741e5f2d62e834
Dottore <patryk.czemierowski@gmail.com>
2020-08-18 13:45:35 +0200

    migracja art

--- VictoriaQilin/VictoriaQilin/settings.py
+++ VictoriaQilin/VictoriaQilin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'art',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'VictoriaQilin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -119,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
--- VictoriaQilin/art/models.py
+++ VictoriaQilin/art/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Obraz(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
